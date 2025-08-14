ec3fe782a28b2ae554953f14e510626352351853
biswarup-capsi <biswarup.naha@capsitech.com>
2025-08-14 15:31:11 +0530

    neon:postgres db integrated with seeding

---
+++ app/components/ChatWidget.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import React,{ useEffect } from 'react';
+
+export default function ChatWidget() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/gh/biswarup-capsi/sv-chatbot-widget@latest/dist/chat-widget.iife.js';
+        script.defer = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return React.createElement('chat-widget');;
+}
