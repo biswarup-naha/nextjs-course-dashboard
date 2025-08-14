f2a8a3a5fc2c6c713e61fa889a4ffe663496c532
biswarup-capsi <biswarup.naha@capsitech.com>
2025-08-14 14:57:10 +0530

    learned how to link between pages and leverage client-side navigation in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -20,14 +20,15 @@ export default function Page() {
             className={styles.shape}
           />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
+            <strong>Welcome to Acme.</strong> This is the example for the {" "}
+            <span className="text-blue-500">
+              Next.js Dashboard webapp
+            </span>
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            // href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
